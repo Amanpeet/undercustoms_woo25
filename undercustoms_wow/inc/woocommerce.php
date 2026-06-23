903ef0c3d9043258e04c34864b1ba56b0f1b052a
Amanz <35286538+Amanpeet@users.noreply.github.com>
2026-06-23 16:13:47 +0530

    woo-thumb fix

--- undercustoms_wow/inc/woocommerce.php
+++ undercustoms_wow/inc/woocommerce.php
@@ -20,8 +20,8 @@ function undercustomz_woocommerce_setup() {
 	add_theme_support(
 		'woocommerce',
 		array(
-			'thumbnail_image_width' => 150,
-			'single_image_width'    => 300,
+			'thumbnail_image_width' => 600,
+			'single_image_width'    => 1200,
 			'product_grid'          => array(
 				'default_rows'    => 3,
 				'min_rows'        => 1,
